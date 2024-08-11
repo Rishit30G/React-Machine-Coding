8de3b3cff6cf5a1488acc32fe77eddbe4cc7e7ff
Rishit Gupta <74411873+Rishit30G@users.noreply.github.com>
2024-08-11 19:45:07 -0400

    update star rating

--- star-rating/src/App.jsx
+++ star-rating/src/App.jsx
@@ -10,16 +10,17 @@ function App() {
 
   return (
     <>
-    <div className='App'>
+    <div className='App'> 
       <h1>
         {[...Array(5)].map((_, index) => {
           const ratingValue = index + 1;
           return (
-          <label key={index}>
-            <input type='radio' name='rating' value={ratingValue} onClick={() => setRating(ratingValue)} />
-             <FaStar className="star" color={ratingValue <= (hover || rating) ? "#ffc107" : "black" } size={100} onMouseEnter={() => setHover(ratingValue)} onMouseLeave={()=>setHover(null)}/>
-          </label>
-        )})}
+            <label key={index}>
+              <input type="radio" name='rating' value={ratingValue} onClick={ () => setRating(ratingValue)}></input>
+              <FaStar className="star" color={ratingValue <= (hover || rating) ? "#ffc107" : "black"} size={100} onMouseEnter={() => setHover(ratingValue)} onMouseLeave={() => setHover(null)} />
+            </label>
+          )
+        })}
       </h1>
       <p>The rating is {rating === null ? 0 : rating}</p>
     </div>
